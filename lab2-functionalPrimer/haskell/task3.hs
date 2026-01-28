983e1fab23f1a535fbeb47fb452e110c299a0e93
Yeasin-512 <240219620@aston.ac.uk>
2026-01-28 19:34:51 +0000

    Changed sgn to sgnGuards

--- lab2-functionalPrimer/haskell/task3.hs
+++ lab2-functionalPrimer/haskell/task3.hs
@@ -4,10 +4,11 @@ sgn x = if x < 0
               then 0 
               else 1
 
-sgnGuards x | x < 0 = -1
+sgnGuards x | x < 0 = 
+            -1
             | x == 0 = 0
             | otherwise = 1
-
+-- seems like they do not need to be on the same line when using guards
 main = do
     putStrLn "Please enter a number"
     input <- getLine 
