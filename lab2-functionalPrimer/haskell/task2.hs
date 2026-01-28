2f1f6eae9430ace326ba7ac4cf2891b0e48c4988
Yeasin-512 <240219620@aston.ac.uk>
2026-01-28 19:22:01 +0000

    Completed task 2 added notes about print and string functions

--- lab2-functionalPrimer/haskell/task2.hs
+++ lab2-functionalPrimer/haskell/task2.hs
@@ -1,4 +1,16 @@
+onePlusone = "1 + 1 = 2"
+test = 'a'
+
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    putStrLn onePlusone;
+    -- To print a string variable, we can use putStrLn function, it also creates a new line.
+    putStr onePlusone;
+    -- To print a string variable, we can also use putStr function, it aldoes not create a new line.
+    putChar test;
+    -- To print a string variable character by character, we can use putChar function needs a '' .
+    -- Can not add Ln to random functions to create a new line.
+    --putStrLn (show unoPlusuno)
+    -- To print non-string variable, we need to convert it to string using show function.
