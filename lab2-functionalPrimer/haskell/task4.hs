25e0d4e44bdce53ec52f99c8660f1aa24861427a
Yeasin-512 <240219620@aston.ac.uk>
2026-01-29 15:05:56 +0000

    Completed task4 added concate and brackets

--- lab2-functionalPrimer/haskell/task4.hs
+++ lab2-functionalPrimer/haskell/task4.hs
@@ -1,17 +1,18 @@
+
 ask :: String -> IO ()
-ask prompt =
+ask prompt  =
 -- function that takes a string and returns an IO action, method name is ask and parameter is prompt
   do
   putStrLn prompt
   line <- getLine
   -- if formatting is important here, if is furthest back to the left, then the then and else/if are indented
   if line == ""
-    then ask prompt
+    then ask (prompt ++ "!" )
     else if line == "quit"
       then putStrLn ("quitting...")
     else do
       putStrLn ("you said: " ++ reverse line )
-      ask prompt
+      ask prompt 
 
 --if line == ""
  -- then ask prompt
@@ -28,5 +29,5 @@ main =
   -- do is required for IO actions / input and output
   let prompt = "please say something"
   -- let just istantiates a variable inside of an IO action
-  ask prompt
+  ask prompt 
   --ask is the name of the function defined above promt is the parameter passed to it
